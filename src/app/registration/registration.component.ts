c769f62a2916f31569d8c6643ede64028a05a783
DaoFof <daofof0gmail.com>
2018-04-23 00:06:55 +0530

    Add registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jQuery :any;
+declare var $ :any;
 
 @Component({
   selector: 'app-registration',
@@ -11,5 +13,15 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  changeView(){
+    $('.tab a').on('click', function (e) {
+      e.preventDefault();      
+      $(this).parent().addClass('active');
+      $(this).parent().siblings().removeClass('active');
+      var target = $(this).attr('data-href');
+      console.log(target);
+      $('.tab-content > div').not(target).hide();
+      $(target).fadeIn(600);
+    });
+  }
 }
